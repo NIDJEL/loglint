4a5cdb1489b1f100c8acba71a227d3e2b6dd8f33
NIDJEL <nikkruglov16@gmail.com>
2026-06-05 20:26:13 +0300

    add lowercase log message rule

--- analyzer/analyzer.go
+++ analyzer/analyzer.go
@@ -2,6 +2,9 @@ package analyzer
 
 import (
 	"go/ast"
+	"go/token"
+	"strconv"
+	"unicode"
 
 	"golang.org/x/tools/go/analysis"
 )
@@ -15,9 +18,74 @@ var Analyzer = &analysis.Analyzer{
 func run(pass *analysis.Pass) (any, error) {
 	for _, file := range pass.Files {
 		ast.Inspect(file, func(node ast.Node) bool {
+			call, ok := node.(*ast.CallExpr)
+			if !ok {
+				return true
+			}
+
+			if !isLogCall(call) {
+				return true
+			}
+
+			msg, ok := firstStingArg(call)
+			if !ok {
+				return true
+			}
+
+			if startsWithUpper(msg) {
+				pass.Reportf(call.Pos(), "log message should start with lowercase letter")
+			}
+
 			return true
 		})
 	}
 
 	return nil, nil
+}
+
+func isLogCall(call *ast.CallExpr) bool {
+	sel, ok := call.Fun.(*ast.SelectorExpr)
+	if !ok {
+		return false
+	}
+
+	switch sel.Sel.Name {
+	case "Debug", "Info", "Warn", "Error":
+		return true
+	default:
+		return false
+	}
+}
+
+func firstStingArg(call *ast.CallExpr) (string, bool) {
+	if len(call.Args) == 0 {
+		return "", false
+	}
+
+	arg, ok := call.Args[0].(*ast.BasicLit)
+	if !ok {
+		return "", false
+	}
+
+	if arg.Kind != token.STRING {
+		return "", false
+	}
+
+	msg, err := strconv.Unquote(arg.Value)
+	if err != nil {
+		return "", false
+	}
+
+	return msg, true
+
+}
+
+func startsWithUpper(msg string) bool {
+	for _, r := range msg {
+		if unicode.IsLetter(r) {
+			return unicode.IsUpper(r)
+		}
+	}
+
+	return false
 }
